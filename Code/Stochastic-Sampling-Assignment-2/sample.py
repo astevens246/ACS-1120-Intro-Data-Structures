d343ac2a75dc1c1cda3381e215a63de40751c228
Allen Stevens <allen.stevens@students.dominican.edu>
2024-01-31 13:51:20 -0800

    Procfile

--- Code/Stochastic-Sampling-Assignment-2/sample.py
+++ Code/Stochastic-Sampling-Assignment-2/sample.py
@@ -14,21 +14,19 @@ import histogram
 # Create a list where each word appears as many times as its frequency
 
 def create_histogram(word_list):
+    # Creates a histogram of word frequencies in a given list of words. 
+    # Returns a dictionary where the keys are words and the values are the frequencies of those words.
+   
     histogram_dict = {}
     for word in word_list:
         histogram_dict[word] = histogram_dict.get(word, 0) + 1
     return histogram_dict
 
 def random_word(histogram):
-    """
-    Selects a random word from the given histogram based on the word frequencies.
+    
+    # Selects a random word from the given histogram based on the word frequencies.
+    # Returns: A randomly selected word from the histogram.
 
-    Args:
-        histogram (dict): A dictionary representing the word frequencies, where the keys are words and the values are their frequencies.
-
-    Returns:
-        str: A randomly selected word from the histogram.
-    """
     frequency_list = []
     for word, frequency in histogram.items():
         frequency_list.extend([word] * frequency)
@@ -40,3 +38,4 @@ word = random_word(histogram)
 
 word_without_punctuation = word.strip('.,;:?!-'"")
 print(word_without_punctuation)
+
